8bff7f335edfe7a682f280167af4901d15b3e3cf
GloryRojas <grojasm94@gmail.com>
2019-07-06 01:15:24 -0500

    Updates

---
+++ precache-manifest.12ea41acc412aa1e6f9cd22d7c8ace30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec0aa2be252f3e6988881579cdccfcc",
+    "url": "/hacking-challenge-rimac/index.html"
+  },
+  {
+    "revision": "0cd7e11100a7f2a1bcf0",
+    "url": "/hacking-challenge-rimac/static/css/main.c6689b8b.chunk.css"
+  },
+  {
+    "revision": "a3f950e032fbf0bde423",
+    "url": "/hacking-challenge-rimac/static/js/2.0ffcbda0.chunk.js"
+  },
+  {
+    "revision": "0cd7e11100a7f2a1bcf0",
+    "url": "/hacking-challenge-rimac/static/js/main.1c498b2e.chunk.js"
+  },
+  {
+    "revision": "f40170edb100bf4b5e27",
+    "url": "/hacking-challenge-rimac/static/js/runtime~main.6bdb7c9e.js"
+  }
+]);
